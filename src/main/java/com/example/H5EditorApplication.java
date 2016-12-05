489763cb53963f3a0b8af741ca38a59c30976681
MrCJ <mr.caojiawang@gmail.com>
2016-12-05 14:01:20 +0800

    First commit

---
+++ src/main/java/com/example/H5EditorApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5EditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H5EditorApplication.class, args);
+	}
+}
